d6835068f1ec09f41abbe7fceb5dd46bdf50866b
martonszel <marton.szel@gmail.com>
2022-05-30 21:03:10 +0200

    file extension

--- Module-3-Javascript-Coding-Task/file-extension.js
+++ Module-3-Javascript-Coding-Task/file-extension.js
@@ -1,10 +1,20 @@
 "use strict";
 
 const getFileExtension = filename => {
-    // your code here
+    const splittedName = filename.split('.')
+    return splittedName.length === 1 || splittedName[0] === '' ? false : splittedName[splittedName.length - 1]
 }
 
 console.log(getFileExtension('data.txt')); // 'txt'
 console.log(getFileExtension('component.test.js')); // 'js'
 console.log(getFileExtension('README')); // false
 console.log(getFileExtension('.git')); // false
+
+
+const searchTerm = '.';
+const extension = (word) => word.indexOf(searchTerm) > 0 ? word.substring(word.lastIndexOf(searchTerm) + 1) : false
+
+console.log(extension('data.txt')); // 'txt'
+console.log(extension('component.test.js')); // 'js'
+console.log(extension('README')); // false
+console.log(extension('.git')); // false
