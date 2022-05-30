1b41e31a009697fd3238593f3ca62887a3f217c8
martonszel <marton.szel@gmail.com>
2022-05-30 17:48:58 +0200

    create skeleton

---
+++ Module-3-Javascript-Coding-Taks/palindrome.js
@@ -0,0 +1,11 @@
+
+const isPalindrome = (text) => {
+    // your code here
+}
+
+console.log(isPalindrome('')); // false
+console.log(isPalindrome('     ')); // false
+console.log(isPalindrome('anna')); // true
+console.log(isPalindrome('anna    ')); // true
+console.log(isPalindrome('indul a gorog aludni')); // true
+console.log(isPalindrome('alma')); // false
