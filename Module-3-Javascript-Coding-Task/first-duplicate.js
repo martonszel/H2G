551305aee12edef73bba906423ed0041183998a0
martonszel <marton.szel@gmail.com>
2022-05-31 16:29:08 +0200

    sum all numbers with flat

--- Module-3-Javascript-Coding-Task/first-duplicate.js
+++ Module-3-Javascript-Coding-Task/first-duplicate.js
@@ -1,7 +1,30 @@
 "use strict";
-const getFirstDuplicate = text => {
-    // your code here
+// const getFirstDuplicate = (text) => {
+//     for (let i = 0; i < text.length; i++) {
+//         if (text.indexOf(text.charAt(i)) !== text.lastIndexOf(text.charAt(i))) {
+//             return text.charAt(i)
+//         }
+//     }
+//     return ''
+// }
+
+// console.log(getFirstDuplicate('abccb')); // 'b'
+// console.log(getFirstDuplicate('abc')); // ''
+
+
+const firstDuplicate = (text) => {
+
+    let tempMap = new Map()
+    for (const char of text) {
+        if (tempMap.has(char)) {
+            return char
+        } else {
+            tempMap.set(char, true)
+            console.log(tempMap);
+        }
+    }
+    return ''
 }
 
-console.log(getFirstDuplicate('abccb')); // 'b'
-console.log(getFirstDuplicate('abc')); // ''
+console.log(firstDuplicate('abccb')); // 'b'
+console.log(firstDuplicate('bcabc')); // 'b'
--- Module-3-Javascript-Coding-Task/sum-all-numbers-nested.js
+++ Module-3-Javascript-Coding-Task/sum-all-numbers-nested.js
@@ -9,5 +9,12 @@ const sumArray = (array) => {
                 sum + current : sum
     }, 0);
 }
-console.log(sumArray([1, [2, [[[3]]]], [4, 5], 100])); // 15
+console.log(sumArray([1, [2, [[[3]]]], [4, 5]])); // 15
 console.log(sumArray([1, [2, [3], [4, '5', null, undefined, [NaN, Infinity], [true, false], { id: '1' }, 5]]])); // 15
+
+
+
+const sumNumbers = (inputArray) => inputArray.flat(4).reduce((sum, current) => typeof current === 'number' && isFinite(current) ? sum + current : sum, 0);
+
+console.log(sumNumbers([1, [2, [[[3]]]], [4, 5]])); // 15
+console.log(sumNumbers([1, [2, [3], [4, '5', null, undefined, [NaN, Infinity], [true, false], { id: '1' }, 5]]])); // 15
