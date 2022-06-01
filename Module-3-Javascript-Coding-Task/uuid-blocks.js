0853903baae3b26a4cec9485d151f0fb4f54ff4c
martonszel <marton.szel@gmail.com>
2022-06-01 16:19:22 +0200

    uuis block#2

--- Module-3-Javascript-Coding-Task/uuid-blocks.js
+++ Module-3-Javascript-Coding-Task/uuid-blocks.js
@@ -16,3 +16,22 @@ const getUUIDBlocks = uuid => {
 }
 
 console.log(getUUIDBlocks('f782f011-636a-4d8b-9f63-dc34a4503c01')); // ['f7', '82', 'f0', '11', '63', '6a', '4d', '8b', '9f', '63', 'dc', '34',' a4', '50', '3c', '01']
+
+
+
+const formatArray = (str, number) => {
+
+    const fromatted = str.replace(/-/g, '')
+
+    let splittedArray = []
+
+    for (let i = 0; i < fromatted.length; i += number) {
+        splittedArray.push(fromatted.slice(i, i + number));
+    }
+
+    return splittedArray
+}
+
+const UUIDBlocks = uuid => formatArray(uuid, 2)
+
+console.log(UUIDBlocks('f782f011-636a-4d8b-9f63-dc34a4503c01')); // ['f7', '82', 'f0', '11', '63', '6a', '4d', '8b', '9f', '63', 'dc', '34',' a4', '50', '3c', '01']
