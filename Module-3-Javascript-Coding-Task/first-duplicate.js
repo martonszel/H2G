178eff26af430f719a0289befc3a5a1c9cb01fa3
martonszel <marton.szel@gmail.com>
2022-06-01 12:24:33 +0200

    first duplicate refact

--- Module-3-Javascript-Coding-Task/first-duplicate.js
+++ Module-3-Javascript-Coding-Task/first-duplicate.js
@@ -3,6 +3,11 @@
 
 const getFirstDuplicate = (text) => {
     for (let i = 0; i < text.length; i++) {
+
+        //          indexof (a)   !==  lastindex (a)
+        //            0                     0  
+        //          indexof (b)   !==  lastindex (b)
+        //              1               4
         if (text.indexOf(text.charAt(i)) !== text.lastIndexOf(text.charAt(i))) {
             return text.charAt(i)
         }
@@ -16,13 +21,18 @@ console.log(getFirstDuplicate('abc')); // ''
 
 const firstDuplicate = (text) => {
 
-    let tempMap = new Map()
-    for (const char of text) {
+    let tempMap = new Map();
+
+    [...text].forEach((el, index) => {
+        tempMap.set(index, el)
+    })
+
+    console.log(tempMap);
+    for (const [key, char] of tempMap) {
         if (tempMap.has(char)) {
             return char
         } else {
             tempMap.set(char, true)
-            console.log(tempMap);
         }
     }
     return ''
@@ -30,6 +40,7 @@ const firstDuplicate = (text) => {
 
 console.log(firstDuplicate('abccb')); // 'b'
 console.log(firstDuplicate('bcabc')); // 'b'
+console.log(firstDuplicate(["1", "2", "3", "4", "3", "2"])); // '2'
 
 
 const findFirstDuplicate = (text) => ([...text].find((element, index) => text.lastIndexOf(element) !== index)) || '';
