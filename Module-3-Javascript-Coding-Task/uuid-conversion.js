19484fd7f6372b9a7a77ab3b02e04b7e4795e9f9
martonszel <marton.szel@gmail.com>
2022-06-02 12:34:21 +0200

    uuid conversion

--- Module-3-Javascript-Coding-Task/uuid-conversion.js
+++ Module-3-Javascript-Coding-Task/uuid-conversion.js
@@ -1,7 +1,18 @@
 "use strict";
 
+
+
 const getConvertedUUIDBlocks = uuid => {
-    // your code here
+
+    const fromatted = [...uuid.replace(/-/g, '')].join('').match(/.{1,2}/g);
+
+    const hexColors = []
+
+    fromatted.forEach(element => {
+        hexColors.push(parseInt(element, 16))
+    });
+
+    return hexColors
 }
 
-console.log(getUUIDBlocks('f782f011-636a-4d8b-9f63-dc34a4503c01')); // [247, 130, 240, 17, 99, 106, 77, 139, 159, 99, 220, 52, 164, 80, 60, 1]
+console.log(getConvertedUUIDBlocks('f782f011-636a-4d8b-9f63-dc34a4503c01')); // [247, 130, 240, 17, 99, 106, 77, 139, 159, 99, 220, 52, 164, 80, 60, 1]
