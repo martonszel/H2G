fff90885fd68832f9ecf6ed427e873575a056271
martonszel <marton.szel@gmail.com>
2022-06-03 10:42:55 +0200

    uuid conversion #2

--- Module-3-Javascript-Coding-Task/uuid-conversion.js
+++ Module-3-Javascript-Coding-Task/uuid-conversion.js
@@ -16,3 +16,10 @@ const getConvertedUUIDBlocks = uuid => {
 }
 
 console.log(getConvertedUUIDBlocks('f782f011-636a-4d8b-9f63-dc34a4503c01')); // [247, 130, 240, 17, 99, 106, 77, 139, 159, 99, 220, 52, 164, 80, 60, 1]
+
+
+// solution #2
+
+const convertedUUIDBlocks = uuid => [...uuid.replace(/-/g, '')].join('').match(/.{1,2}/g).map((element) => element = parseInt(element, 16))
+
+console.log(convertedUUIDBlocks('f782f011-636a-4d8b-9f63-dc34a4503c01')); // [247, 130, 240, 17, 99, 106, 77, 139, 159, 99, 220, 52, 164, 80, 60, 1]
