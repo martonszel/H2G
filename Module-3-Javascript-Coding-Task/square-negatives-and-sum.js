38132d0163df996c705f217d4de186cb9070546e
martonszel <marton.szel@gmail.com>
2022-06-07 10:51:31 +0200

    square and sum

--- Module-3-Javascript-Coding-Task/square-negatives-and-sum.js
+++ Module-3-Javascript-Coding-Task/square-negatives-and-sum.js
@@ -27,3 +27,25 @@ const squareAndSum = (array) => {
 }
 
 console.log(squareAndSum([-1, 1, 12, -4, -5, 999])); // [1, 1, 3, 7, 7, 9]
+
+
+// const sumAndSquare = (array) => {
+//     return array.map((element) => element < 0 ? Math.pow(element, 2) : element)
+//         .map((item) => item.toString().length < 2 ? item : [...item.toString()].reduce((total, current) => {
+//             return total + Number(current)
+//         }, 0))
+
+// }
+
+// console.log(sumAndSquare([-1, 1, 12, -4, -5, 999])); // [1, 1, 3, 7, 7, 9]
+
+
+const sumAndSquare = (array) =>
+    array.map((element) => element < 0 ? Math.pow(element, 2) : element)
+        .map((item) => item % 9 || 9)
+
+
+console.log(sumAndSquare([-1, 1, 12, -6, -5, 999])); // [1, 1, 3, 7, 7, 9]
+
+
+
