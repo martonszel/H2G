9e282583ec7d2d26ca2ff6996f45d6991cfbdb6a
martonszel <marton.szel@gmail.com>
2022-06-17 12:46:29 +0200

    try catch error isloading

--- React-Dojo/react_typescript/src/pages/ClassBased.tsx
+++ React-Dojo/react_typescript/src/pages/ClassBased.tsx
@@ -15,12 +15,16 @@ type Props = {
 
 type State = {
     characters: any[];
+    error: string | null,
+    isLoaded: boolean,
 }
 
 export default class ClassBased extends React.Component<Props, State> {
 
     state: State = {
         characters: [],
+        error: null,
+        isLoaded: false,
     }
 
     componentDidMount() {
@@ -28,43 +32,55 @@ export default class ClassBased extends React.Component<Props, State> {
     }
 
     getAllCharacters = async () => {
-        const response = await fetch(`${this.props.apiUrl}?limit=21?${this.props.apiKey}`)
-        const data = await response.json()
+        try {
+            const response = await fetch(`${this.props.apiUrl}?limit=21?${this.props.apiKey}`)
+            const data = await response.json()
 
-        this.setState({ characters: data.data.results });
-        console.log(this.state.characters);
+            this.setState({ characters: data.data.results, isLoaded: true });
+        } catch (error) {
+            if (error instanceof Error) {
+                this.setState({ error: error.message, isLoaded: true });
+            }
+        }
     }
 
     render() {
-        return (
-            <div className='classBased'>
-                <h1>Classbased</h1>
-
-                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                    {this.state.characters.map((char) =>
-                        <Grid item xs={2} sm={4} md={4} key={char.id} >
-                            <Card sx={{ maxWidth: 345 }}>
-                                <CardMedia
-                                    component="img"
-                                    height="250"
-                                    image={char.thumbnail.path + '.' + char.thumbnail.extension}
-                                    alt={char.name}
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div" >
-                                        {char.name}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary" height="10em" >
-                                        {char.description.length > 0 ? char.description : 'no description'}
-                                    </Typography>
-                                </CardContent>
-                                <CardActions>
-                                    <Button size="small"><a href={char.urls[0].url} target="_blank">Learn more</a></Button>
-                                </CardActions>
-                            </Card>
-                        </Grid>)}
-                </Grid>
-            </div >
-        )
+        if (this.state.error) {
+            return <div>
+                <h1>Error: {this.state.error}</h1>
+            </div>
+        } else if (!this.state.isLoaded) {
+            return <div className='classBased'><h1>Loading...</h1></div>
+        } else {
+            return (
+                <div className='classBased'>
+                    <h1>Classbased</h1>
+                    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                        {this.state.characters.map((char) =>
+                            <Grid item xs={2} sm={4} md={4} key={char.id} >
+                                <Card sx={{ maxWidth: 345 }}>
+                                    <CardMedia
+                                        component="img"
+                                        height="250"
+                                        image={char.thumbnail.path + '.' + char.thumbnail.extension}
+                                        alt={char.name}
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div" >
+                                            {char.name}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary" height="10em" >
+                                            {char.description.length > 0 ? char.description : 'no description'}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small"><a href={char.urls[0].url} target="_blank">Learn more</a></Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>)}
+                    </Grid>
+                </div >
+            );
+        }
     }
 }
