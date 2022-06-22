bbff694849fc8e9e575ff76de1858fa54a6b244c
martonszel <marton.szel@gmail.com>
2022-06-22 15:14:46 +0200

    header layout

--- React-Dojo/netflix_clone_react/src/components/Header.tsx
+++ React-Dojo/netflix_clone_react/src/components/Header.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 // const photo = require('../assets/logo.png');
 import logo from '../assets/logo.png'
+import SearchBar from './SearchBar'
+import classes from "./Header.module.css";
 
 type Props = {}
 
 const Header = (props: Props) => {
     return (
-        <div>
-            <h1>header</h1>
-            <img src={logo} alt="Netflix Logo" />
+        <div className={classes.headerContainer}>
+            <div className={classes.imageArea}></div>
+            <div className={classes.content}>
+                <img className={classes.logo} src={logo} alt="Netflix Logo" />
+                <button className={classes.button}>+Add Movie</button>
+            </div>
+            <SearchBar />
+
         </div>
     )
 }
