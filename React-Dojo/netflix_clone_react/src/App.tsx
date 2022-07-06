d8eb12a2083a295f6ddac71e9da8bde5ce5441dc
martonszel <marton.szel@gmail.com>
2022-07-06 11:06:16 +0200

    adding routes

--- React-Dojo/netflix_clone_react/src/App.tsx
+++ React-Dojo/netflix_clone_react/src/App.tsx
@@ -1,43 +1,23 @@
 
 import './App.css';
 import { useState } from "react";
-import Footer from './components/Footer';
-import Header from './components/Header';
-import MainPage from './pages/MainPage';
+
 import Login from './pages/Login';
-import MovieDetails from './components/MovieDetails';
-import { useAppSelector } from './store/hooks'
-import { useFetchMoviesQuery } from './features/movie-api-slice';
+import Dashboard from './pages/Dashboard';
+
+import { Routes, Route, Link } from "react-router-dom";
 
 const App: React.FC = () => {
 
   // eslint-disable-next-line
   const [isLoggedIn, setisLoggedIn] = useState<boolean>(true)
 
-  const oneMovie = useAppSelector((state) => state.movie.oneMovie)
-  const { data = [], isFetching } = useFetchMoviesQuery()
-
   return (
     <div className="App" >
-      {
-        isLoggedIn ? (
-          <div className="container">
-            {oneMovie.id &&
-              <MovieDetails />
-            }
-            {!oneMovie.id &&
-              <Header />
-            }
-
-            <MainPage />
-            <Footer />
-          </div>
-        ) : (
-          <div className="container">
-            <Login />
-          </div>
-        )
-      }
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </div >
   );
 }
