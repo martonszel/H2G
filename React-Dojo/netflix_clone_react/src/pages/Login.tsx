cb459dc9276608dd0186ca722768b20b1401b199
martonszel <marton.szel@gmail.com>
2022-07-29 10:13:09 +0200

    merge conflicts

--- React-Dojo/netflix_clone_react/src/pages/Login.tsx
+++ React-Dojo/netflix_clone_react/src/pages/Login.tsx
@@ -12,61 +12,59 @@ const Login = () => {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
 
+    const [addUser, result] = useLoginUserMutation();
+    const dispatch = useAppDispatch()
+    const navigate = useNavigate();
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        const [addUser, result] = useLoginUserMutation();
-        const dispatch = useAppDispatch()
-        const navigate = useNavigate();
-
-        const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-            e.preventDefault();
-            const user = {
-                email, password
-            }
-            console.log(user);
-            await addUser(user);
-            dispatch(setUser)
-            navigate("/private");
-
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const user = {
+            email, password
         }
-
-        const resetForm = () => {
-            setEmail('')
-            setPassword('')
-        }
-
-
-        return (
-
-            <div className={classes.wrap}>
-                <form className={classes.loginForm} onSubmit={(e) => handleSubmit(e)} >
-                    <h1>Login</h1>
-
-                    <div className={classes.formGroup}>
-                        <label htmlFor='email'>Email</label>
-                        <input type="email" id="email" required className={classes.formInput} value={email} onChange={(e) => setEmail(e.currentTarget.value)} placeholder='user@user.com' />
-                    </div>
-
-                    <div className={classes.formGroup}>
-                        <label htmlFor='password'>Password</label>
-                        <input type="password" id="password" required className={classes.formInput} value={password} onChange={(e) => setPassword(e.currentTarget.value)} placeholder='*******' />
-                    </div>
-
-                    <div className={classes.formFooter} >
-                        <div className={classes.formButton}>
-                            <button type="reset" onClick={() => resetForm()} >Reset</button>
-                        </div>
-
-                        <div className={classes.formButton}>
-                            <button type="submit">Login</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-
-        )
+        console.log(user);
+        await addUser(user);
+        dispatch(setUser)
+        navigate("/private");
 
     }
+
+    const resetForm = () => {
+        setEmail('')
+        setPassword('')
+    }
+
+
+    return (
+
+        <div className={classes.wrap}>
+            <form className={classes.loginForm} onSubmit={(e) => handleSubmit(e)} >
+                <h1>Login</h1>
+
+                <div className={classes.formGroup}>
+                    <label htmlFor='email'>Email</label>
+                    <input type="email" id="email" required className={classes.formInput} value={email} onChange={(e) => setEmail(e.currentTarget.value)} placeholder='user@user.com' />
+                </div>
+
+                <div className={classes.formGroup}>
+                    <label htmlFor='password'>Password</label>
+                    <input type="password" id="password" required className={classes.formInput} value={password} onChange={(e) => setPassword(e.currentTarget.value)} placeholder='*******' />
+                </div>
+
+                <div className={classes.formFooter} >
+                    <div className={classes.formButton}>
+                        <button type="reset" onClick={() => resetForm()} >Reset</button>
+                    </div>
+
+                    <div className={classes.formButton}>
+                        <button type="submit">Login</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    )
+
 }
+
 
 export default Login
